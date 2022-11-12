d5ee2b1f68b24da33528f54d16ce830ad162da05
Majoro Futhoane <85506666+AzazelEmjay@users.noreply.github.com>
2022-11-12 17:28:02 +0200

    changing changing database_url to mongodb_uri

--- docs/endpointsdocs.js
+++ docs/endpointsdocs.js
@@ -1,1 +1,53 @@
-//
+// Login endpoint Documentation start here
+
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     tags:
+ *       - Login
+ *     description: Login
+ *     parameters:
+ *       - name: reqBody
+ *         description: request body
+ *         in: body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             username:
+ *               type: string
+ *             password:
+ *               type: string
+ *           required:
+ *             - username
+ *             - password
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ */
+// Login endpoint Documentation end here
+
+
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     tags:
+ *       - Register new user
+ *     description: Post to user database
+ *     parameters:
+ *       - name: reqBody
+ *         description: request body
+ *         in: body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             text:
+ *               type: string
+ *           required:
+ *             - text
+ *     responses:
+ *       '200':
+ *         description: A successful response
+ */
+// Create Category endpoint documentation start here
--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const uri = process.env.MONGODB_URI;
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(uri, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error)=> console.error(error))
 db.once('open',(error)=> console.log('Connected to Database'))
